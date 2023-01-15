10dcb1aa3fc71ed54ba4122d7b898663dc629b46
SerhiiRodin <rodinserj@ukr.net>
2023-01-15 13:17:35 +0200

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
+const buttonEl = document.querySelector('button');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (email === '' || password === '') {
+    alert('Усі поля форми повинні бути заповненні.');
+  } else {
+    const dataForm = {
+      email,
+      password,
+    };
+    console.log(dataForm);
+
+    event.currentTarget.reset();
+  }
+}
